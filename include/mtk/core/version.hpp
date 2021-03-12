fa6608cc339dd55c5e2a692f9aef6e0d1aa2c8a8
Mestkon <havhoi92@gmail.com>
2021-03-12 14:51:35 +0100

    version 0.1

--- include/mtk/core/version.hpp
+++ include/mtk/core/version.hpp
@@ -2,7 +2,7 @@
 #define MTK_CORE_VERSION_HPP
 
 #define MTK_MAJOR_VERSION 0
-#define MTK_MINOR_VERSION 0
+#define MTK_MINOR_VERSION 1
 
 //! Combined version. Upper 16 bits is major, lower 16 bits is minor.
 #define MTK_VERSION ((MTK_MAJOR_VERSION << 16) | MTK_MINOR_VERSION)
