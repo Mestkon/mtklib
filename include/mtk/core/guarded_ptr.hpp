045a2e9df3bcc8694b87bf81c04b5fd17997dc8f
Mestkon <havhoi92@gmail.com>
2021-03-15 02:06:23 +0100

    adopt unique_ptr interface

--- include/mtk/core/guarded_ptr.hpp
+++ include/mtk/core/guarded_ptr.hpp
@@ -2,6 +2,8 @@
 #define MTK_CORE_GUARDED_PTR_HPP
 
 #include <mtk/core/types.hpp>
+#include <mtk/core/impl/actor_base.hpp>
+#include <mtk/core/impl/move.hpp>
 #include <mtk/core/impl/pointer_validators.hpp>
 #include <mtk/core/impl/require.hpp>
 
@@ -12,37 +14,123 @@ namespace mtk {
 template<class T
 	,class Validator = default_pointer_validator>
 class guarded_ptr :
-	Validator
+	actor_base<Validator>
 {
-	static_assert(std::is_nothrow_default_constructible_v<Validator>, "Validator must be nothrow default constructible");
+	static_assert(!std::is_reference_v<Validator>, "Validator cannot be a reference type");
 
-	static constexpr bool _nothrow_validator = std::is_nothrow_invocable_v<Validator, T*>;
+	static constexpr bool _nothrow_validator_invoke = std::is_nothrow_invocable_v<Validator, T*>;
+	static constexpr bool _nothrow_validator_default = std::is_nothrow_default_constructible_v<Validator>;
 	static constexpr bool _nothrow_validator_copy = std::is_nothrow_copy_constructible_v<Validator>;
+	static constexpr bool _nothrow_validator_move = std::is_nothrow_move_constructible_v<Validator>;
+
+	using _type = std::remove_extent_t<T>;
 
 public:
-	using pointer = T*;
-	using element_type = T;
+	using pointer = _type*;
+	using element_type = _type;
 	using validator_type = Validator;
 
+#ifndef MTK_DOXYGEN
+	template<class V = validator_type
+		,_require<!std::is_pointer_v<V>> = 0
+		,_require<std::is_default_constructible_v<V>> = 0>
+#endif
 	constexpr
-	guarded_ptr(const Validator& v = Validator()) noexcept(_nothrow_validator_copy) :
+	guarded_ptr() noexcept(_nothrow_validator_default && _nothrow_validator_copy) :
+		guarded_ptr(nullptr, validator_type())
+	{ }
+
+	constexpr
+	guarded_ptr(const validator_type& v) noexcept(_nothrow_validator_copy) :
 		guarded_ptr(nullptr, v)
 	{ }
 
 	constexpr
-	guarded_ptr(pointer p, const Validator& v = Validator()) noexcept(_nothrow_validator_copy) :
-		Validator(v),
+	guarded_ptr(validator_type&& v) noexcept(_nothrow_validator_move) :
+		guarded_ptr(nullptr, mtk::_move(v))
+	{ }
+
+	constexpr
+	guarded_ptr(std::nullptr_t) noexcept(_nothrow_validator_default) :
+		guarded_ptr(nullptr, validator_type())
+	{ }
+
+	template<class U
+#ifndef MTK_DOXYGEN
+		,_require<(!std::is_array_v<T> && std::is_convertible_v<U*, element_type*>)
+			|| std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+		,class V = validator_type
+		,_require<!std::is_pointer_v<V>> = 0
+		,_require<std::is_default_constructible_v<V>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr(U* p) noexcept(_nothrow_validator_default && _nothrow_validator_copy) :
+		guarded_ptr(p, validator_type())
+	{ }
+
+	template<class U
+#ifndef MTK_DOXYGEN
+	,_require<(!std::is_array_v<T> && std::is_convertible_v<U*, element_type*>)
+			|| std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr(U* p, const validator_type& v) noexcept(_nothrow_validator_copy) :
+		actor_base<Validator>(v),
 		m_ptr(p)
+	{ }
+
+	template<class U
+#ifndef MTK_DOXYGEN
+	,_require<(!std::is_array_v<T> && std::is_convertible_v<U*, element_type*>)
+			|| std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr(U* p, validator_type&& v) noexcept(_nothrow_validator_move) :
+		actor_base<Validator>(mtk::_move(v)),
+		m_ptr(p)
+	{ }
+
+	constexpr
+	guarded_ptr(std::nullptr_t, const validator_type& v) noexcept(_nothrow_validator_copy) :
+		actor_base<Validator>(v),
+		m_ptr(nullptr)
+	{ }
+
+	constexpr
+	guarded_ptr(std::nullptr_t, validator_type&& v) noexcept(_nothrow_validator_move) :
+		actor_base<Validator>(mtk::_move(v)),
+		m_ptr(nullptr)
 	{ }
 
 	template<class U
 		,class V
 #ifndef MTK_DOXYGEN
-		,_require<std::is_convertible_v<U*, T*>> = 0
+		,class Tp = T
+		,_require<!std::is_array_v<Tp>> = 0
+		,_require<!std::is_array_v<U>> = 0
+		,_require<std::is_convertible_v<U*, pointer>> = 0
 #endif
 	>
-	guarded_ptr(guarded_ptr<U, V> p, const Validator& v = Validator()) noexcept(_nothrow_validator_copy) :
-		guarded_ptr(p.get(), v)
+	constexpr
+	guarded_ptr(guarded_ptr<U, V> p) noexcept(_nothrow_validator_copy) :
+		guarded_ptr(p.get(), p.get_validator())
+	{ }
+
+	template<class U
+		,class V
+#ifndef MTK_DOXYGEN
+		,class Tp
+		,_require<std::is_array_v<Tp>> = 0
+		,_require<std::is_array_v<U>> = 0
+		,_require<std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr(guarded_ptr<U, V> p) noexcept(_nothrow_validator_copy) :
+		guarded_ptr(p.get(), p.get_validator())
 	{ }
 
 
@@ -51,34 +139,111 @@ public:
 	guarded_ptr&
 	operator=(pointer rhs) noexcept { m_ptr = rhs; return *this; }
 
+	template<class U
+		,class V
+#ifndef MTK_DOXYGEN
+		,class Tp = T
+		,_require<!std::is_array_v<Tp>> = 0
+		,_require<!std::is_array_v<U>> = 0
+		,_require<std::is_convertible_v<U*, pointer>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr&
+	operator=(guarded_ptr<U, V> rhs) noexcept(_nothrow_validator_copy)
+	{
+		m_ptr = rhs.get();
+		this->_get_actor() = rhs.get_validator();
+		return *this;
+	}
+
+	template<class U
+		,class V
+#ifndef MTK_DOXYGEN
+		,class Tp
+		,_require<std::is_array_v<Tp>> = 0
+		,_require<std::is_array_v<U>> = 0
+		,_require<std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+#endif
+	>
+	constexpr
+	guarded_ptr&
+	operator=(guarded_ptr<U, V> rhs) noexcept(_nothrow_validator_copy)
+	{
+		m_ptr = rhs.get();
+		this->_get_actor() = rhs.get_validator();
+		return *this;
+	}
+
+
+
 	constexpr
 	operator T*() const noexcept { return m_ptr; }
 
 	constexpr
 	element_type&
-	operator*() const noexcept(_nothrow_validator) { this->_verify(); return *m_ptr; }
+	operator*() const noexcept(_nothrow_validator_invoke) { this->_verify(); return *m_ptr; }
 
 	constexpr
 	pointer
-	operator->() const noexcept(_nothrow_validator) { this->_verify(); return m_ptr; }
+	operator->() const noexcept(_nothrow_validator_invoke) { this->_verify(); return m_ptr; }
 
+#ifndef MTK_DOXYGEN
+	template<class U = T
+		,_require<std::is_same_v<U, T>> = 0
+		,_require<std::is_array_v<U>> = 0>
+#endif
 	constexpr
 	element_type&
-	operator[](std::size_t idx) const noexcept(_nothrow_validator) { this->_verify(); return *(m_ptr + idx); }
+	operator[](size_t idx) const noexcept(_nothrow_validator_invoke) { this->_verify(); return *(m_ptr + idx); }
+
+#ifndef MTK_DOXYGEN
+	template<class U = T
+		,_require<std::is_same_v<U, T>> = 0
+		,_require<!std::is_array_v<U>> = 0>
+#endif
+	constexpr
+	element_type&
+	operator[](size_t idx) const noexcept = delete;
 
 
+
+	template<class U
+#ifndef MTK_DOXYGEN
+	,_require<(!std::is_array_v<T> && std::is_convertible_v<U*, element_type*>)
+			|| std::is_convertible_v<U(*)[], element_type(*)[]>> = 0
+#endif
+	>
+	constexpr
+	void
+	reset(U* p) noexcept { m_ptr = p; }
+
+	constexpr
+	void
+	reset(std::nullptr_t = nullptr) noexcept { m_ptr = nullptr; }
 
 	pointer
 	get() const noexcept { return m_ptr; }
 
 	const validator_type&
-	validator() const noexcept { return static_cast<const Validator&>(*this); }
+	get_validator() const noexcept { return this->_get_actor(); }
+
+	validator_type&
+	get_validator() noexcept { return this->_get_actor(); }
+
+	void
+	swap(guarded_ptr& other) noexcept(std::is_nothrow_swappable_v<validator_type>)
+	{
+		using std::swap;
+		swap(m_ptr, other.m_ptr);
+		swap(this->get_validator(), other.get_validator());
+	}
 
 
 
 	friend constexpr
 	guarded_ptr&
-	operator++(guarded_ptr& rhs) noexcept(_nothrow_validator)
+	operator++(guarded_ptr& rhs) noexcept(_nothrow_validator_invoke)
 	{
 		rhs._verify();
 		++rhs.m_ptr;
@@ -87,7 +252,7 @@ public:
 
 	friend constexpr
 	guarded_ptr
-	operator++(guarded_ptr& lhs, int) noexcept(_nothrow_validator)
+	operator++(guarded_ptr& lhs, int) noexcept(_nothrow_validator_invoke)
 	{
 		auto cp = lhs;
 		++lhs;
@@ -96,7 +261,7 @@ public:
 
 	friend constexpr
 	guarded_ptr&
-	operator--(guarded_ptr& rhs) noexcept(_nothrow_validator)
+	operator--(guarded_ptr& rhs) noexcept(_nothrow_validator_invoke)
 	{
 		rhs._verify();
 		--rhs.m_ptr;
@@ -105,7 +270,7 @@ public:
 
 	friend constexpr
 	guarded_ptr
-	operator--(guarded_ptr& lhs, int) noexcept(_nothrow_validator)
+	operator--(guarded_ptr& lhs, int) noexcept(_nothrow_validator_invoke)
 	{
 		auto cp = lhs;
 		--lhs;
@@ -121,7 +286,7 @@ public:
 #else
 	std::enable_if_t<std::is_integral_v<U>, guarded_ptr&>
 #endif
-	operator+=(guarded_ptr& lhs, U rhs) noexcept(_nothrow_validator)
+	operator+=(guarded_ptr& lhs, U rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs._verify();
 		lhs.m_ptr += rhs;
@@ -135,7 +300,7 @@ public:
 #else
 	std::enable_if_t<std::is_integral_v<U>, guarded_ptr&>
 #endif
-	operator-=(guarded_ptr& lhs, U rhs) noexcept(_nothrow_validator)
+	operator-=(guarded_ptr& lhs, U rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs._verify();
 		lhs.m_ptr -= rhs;
@@ -149,7 +314,7 @@ public:
 #else
 	std::enable_if_t<std::is_integral_v<U>, guarded_ptr>
 #endif
-	operator+(guarded_ptr lhs, U rhs) noexcept(_nothrow_validator)
+	operator+(guarded_ptr lhs, U rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs += rhs;
 		return lhs;
@@ -162,7 +327,7 @@ public:
 #else
 	std::enable_if_t<std::is_integral_v<U>, guarded_ptr>
 #endif
-	operator+(U lhs, guarded_ptr rhs) noexcept(_nothrow_validator)
+	operator+(U lhs, guarded_ptr rhs) noexcept(_nothrow_validator_invoke)
 	{
 		rhs += lhs;
 		return rhs;
@@ -175,7 +340,7 @@ public:
 #else
 	std::enable_if_t<std::is_integral_v<U>, guarded_ptr>
 #endif
-	operator-(guarded_ptr lhs, U rhs) noexcept(_nothrow_validator)
+	operator-(guarded_ptr lhs, U rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs -= rhs;
 		return lhs;
@@ -183,7 +348,7 @@ public:
 
 	friend constexpr
 	ptrdiff_t
-	operator-(guarded_ptr lhs, guarded_ptr rhs) noexcept(_nothrow_validator)
+	operator-(guarded_ptr lhs, guarded_ptr rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs._verify();
 		rhs._verify();
@@ -192,7 +357,7 @@ public:
 
 	friend  constexpr
 	ptrdiff_t
-	operator-(guarded_ptr lhs, pointer rhs) noexcept(_nothrow_validator)
+	operator-(guarded_ptr lhs, pointer rhs) noexcept(_nothrow_validator_invoke)
 	{
 		lhs._verify();
 		return (lhs.m_ptr - rhs);
@@ -200,20 +365,30 @@ public:
 
 	friend constexpr
 	ptrdiff_t
-	operator-(pointer lhs, guarded_ptr rhs) noexcept(_nothrow_validator)
+	operator-(pointer lhs, guarded_ptr rhs) noexcept(_nothrow_validator_invoke)
 	{
 		rhs._verify();
 		return (lhs - rhs.m_ptr);
 	}
 
 private:
-	void _verify() const noexcept(_nothrow_validator)
+	void _verify() const noexcept(_nothrow_validator_invoke)
 	{
-		this->validator()(m_ptr);
+		this->get_validator()(m_ptr);
 	}
 
 	pointer m_ptr;
 };
+
+
+
+template<class T
+	,class V>
+void
+swap(guarded_ptr<T, V>& a, guarded_ptr<T, V>& b) noexcept(noexcept(a.swap(b)))
+{
+	a.swap(b);
+}
 
 } // namespace mtk
 
