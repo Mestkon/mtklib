4e5cc997304342b3b863771c290c6dabc289ea2c
Mestkon <havhoi92@gmail.com>
2021-04-13 11:05:06 +0200

    add zstring_view

---
+++ include/mtk/core/zstring_view.hpp
@@ -0,0 +1,240 @@
+#ifndef MTK_CORE_ZSTRING_VIEW_HPP
+#define MTK_CORE_ZSTRING_VIEW_HPP
+
+#include <mtk/core/assert.hpp>
+#include <mtk/core/types.hpp>
+#include <mtk/core/impl/declval.hpp>
+#include <mtk/core/impl/require.hpp>
+
+#include <cstring>
+#include <iosfwd>
+#include <type_traits>
+
+namespace mtk {
+
+#ifdef MTK_DEBUG
+constexpr
+size_t
+_constexpr_strlen(const char* s) noexcept
+{
+	const char* e = s;
+	while (*(e++) != '\0');
+	return static_cast<size_t>(e - s - 1);
+}
+#endif
+
+class zstring_view
+{
+public:
+	using value_type = char;
+	using pointer = char*;
+	using const_pointer = const char*;
+	using reference = char&;
+	using const_reference = const char&;
+	using iterator = const_pointer;
+	using const_iterator = const_pointer;
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+
+	constexpr
+	zstring_view() noexcept :
+		m_ptr(nullptr),
+		m_size(0)
+	{ }
+
+	constexpr
+	zstring_view(const char* ptr, size_type count) :
+		m_ptr(ptr),
+		m_size(count)
+	{
+		MTK_ASSERT(ptr != nullptr);
+		MTK_ASSERT(count == _constexpr_strlen(ptr));
+	}
+
+	zstring_view(const char* ptr) :
+		m_ptr(ptr),
+		m_size(0)
+	{
+		MTK_ASSERT(ptr != nullptr);
+		m_size = std::strlen(ptr);
+	}
+
+	template<class StdString
+#ifndef MTK_DOXYGEN
+		//check for random std::string specific interface
+		,typename StdString::traits_type* = nullptr
+		,typename StdString::allocator_type* = nullptr
+		,_require<std::is_constructible_v<StdString, const char*>> = 0
+		,_require<std::is_same_v<decltype(mtk::_declval<StdString&>().c_str()), const char*>> = 0
+		,decltype(mtk::_declval<StdString&>().length())* = nullptr
+		,std::remove_reference_t<decltype(mtk::_declval<StdString&>().replace(size_t(), size_t(), mtk::_declval<StdString&>()))>* = nullptr
+		,decltype(mtk::_declval<StdString&>().find_first_not_of(mtk::_declval<StdString&>()))* = nullptr
+		,decltype(StdString::npos)* = nullptr
+		,std::remove_reference_t<decltype(mtk::_declval<StdString&>() += mtk::_declval<StdString&>())>* = nullptr
+		,std::remove_reference_t<decltype(mtk::_declval<StdString&>() += "")>* = nullptr
+#endif
+	>
+	zstring_view(const StdString& s) :
+		m_ptr(s.c_str()),
+		m_size(s.size())
+	{ }
+
+	constexpr
+	const_iterator
+	begin() const noexcept
+	{
+		return this->data();
+	}
+
+	constexpr
+	const_iterator
+	cbegin() const noexcept
+	{
+		return this->begin();
+	}
+
+	constexpr
+	const_iterator
+	end() const noexcept
+	{
+		return this->data() + this->size();
+	}
+
+	constexpr
+	const_iterator
+	cend() const noexcept
+	{
+		return this->end();
+	}
+
+	constexpr
+	const_reference
+	operator[](size_type pos) const
+	{
+		MTK_ASSERT(pos < this->size());
+		return *(this->begin() + pos);
+	}
+
+	constexpr
+	const_reference
+	front() const
+	{
+		MTK_ASSERT(!this->empty());
+		return *this->begin();
+	}
+
+	constexpr
+	const_reference
+	back() const
+	{
+		MTK_ASSERT(!this->empty());
+		return *(this->end() - 1);
+	}
+
+	constexpr
+	const_pointer
+	data() const noexcept
+	{
+		return m_ptr;
+	}
+
+	constexpr
+	size_type
+	size() const noexcept
+	{
+		return m_size;
+	}
+
+	constexpr
+	size_type
+	length() const noexcept
+	{
+		return m_size;
+	}
+
+	constexpr
+	size_type
+	max_size() const noexcept
+	{
+		return static_cast<size_type>(-1) - 1;
+	}
+
+	[[nodiscard]]
+	constexpr
+	bool
+	empty() const noexcept
+	{
+		return (this->size() == 0);
+	}
+
+	constexpr
+	void
+	swap(zstring_view& v) noexcept
+	{
+		const auto p = m_ptr;
+		const auto s = m_size;
+		m_ptr = v.m_ptr;
+		m_size = v.m_size;
+		v.m_ptr = p;
+		v.m_size = s;
+	}
+
+private:
+	const_pointer m_ptr;
+	size_type m_size;
+};
+
+bool
+operator==(zstring_view lhs, zstring_view rhs) noexcept;
+
+inline
+bool
+operator!=(zstring_view lhs, zstring_view rhs) noexcept
+{
+	return !(lhs == rhs);
+}
+
+bool
+operator<(zstring_view lhs, zstring_view rhs) noexcept;
+
+inline
+bool
+operator>(zstring_view lhs, zstring_view rhs) noexcept
+{
+	return (rhs < lhs);
+}
+
+inline
+bool
+operator<=(zstring_view lhs, zstring_view rhs) noexcept
+{
+	return !(rhs < lhs);
+}
+
+inline
+bool
+operator>=(zstring_view lhs, zstring_view rhs) noexcept
+{
+	return !(lhs < rhs);
+}
+
+std::ostream&
+operator<<(std::ostream& os, zstring_view zv);
+
+inline namespace literals {
+inline namespace zstring_view_literals {
+
+constexpr
+zstring_view
+operator""_zv(const char* str, size_t len) noexcept
+{
+	return zstring_view(str, len);
+}
+
+
+} // namespace zstring_view_literals
+} // namespace literals
+
+} // namespace mtk
+
+#endif
---
+++ src/mtk/core/zstring_view.cpp
@@ -0,0 +1,44 @@
+#include <mtk/core/zstring_view.hpp>
+
+#include <iostream>
+
+namespace mtk {
+
+bool
+operator==(zstring_view lhs, zstring_view rhs) noexcept
+{
+	if (lhs.size() != rhs.size())
+		return false;
+
+	if (!lhs.data() && !rhs.data())
+		return true;
+
+	if (bool(lhs.data()) ^ bool(rhs.data()))
+		return false;
+
+	return (std::strcmp(lhs.data(), rhs.data()) == 0);
+}
+
+bool
+operator<(zstring_view lhs, zstring_view rhs) noexcept
+{
+	if (!lhs.data() && !rhs.data())
+		return false;
+
+	if (!lhs.data() && rhs.data())
+		return true;
+
+	if (lhs.data() && !rhs.data())
+		return false;
+
+	return (std::strcmp(lhs.data(), rhs.data()) < 0);
+}
+
+std::ostream&
+operator<<(std::ostream& os, zstring_view zv)
+{
+	os << zv.data();
+	return os;
+}
+
+} // namespace mtk
