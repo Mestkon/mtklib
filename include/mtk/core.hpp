d853e7febada29b5ccddfba7d17bfdf9d0f27d01
Mestkon <havhoi92@gmail.com>
2021-04-18 02:55:18 +0200

    add core.hpp to include the core module

---
+++ include/mtk/core.hpp
@@ -0,0 +1,18 @@
+#ifndef MTK_CORE_HPP
+#define MTK_CORE_HPP
+
+#include <mtk/core/array.hpp>
+#include <mtk/core/assert.hpp>
+#include <mtk/core/byte_cast.hpp>
+#include <mtk/core/narrow_cast.hpp>
+#include <mtk/core/not_null.hpp>
+#include <mtk/core/nullptr_exception.hpp>
+#include <mtk/core/pointer.hpp>
+#include <mtk/core/saturate_cast.hpp>
+#include <mtk/core/span.hpp>
+#include <mtk/core/types.hpp>
+#include <mtk/core/unique_ptr.hpp>
+#include <mtk/core/version.hpp>
+#include <mtk/core/zstring_view.hpp>
+
+#endif
