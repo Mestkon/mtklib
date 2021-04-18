6d94e8b9169af296d54898b7e1f3e445f2b086c7
Mestkon <havhoi92@gmail.com>
2021-04-18 03:13:22 +0200

    add flag_operators

---
+++ include/mtk/core/flag_operators.hpp
@@ -0,0 +1,40 @@
+#ifndef MTK_CORE_FLAG_OPERATORS_HPP
+#define MTK_CORE_FLAG_OPERATORS_HPP
+
+#include <mtk/core/preprocessor.hpp>
+
+#include <type_traits>
+
+namespace mtk {
+
+#define MTK_IMPL_FLAG_OPERATOR(ENUM, OP) \
+	constexpr \
+	ENUM \
+	operator OP (ENUM lhs, ENUM rhs) noexcept \
+	{ \
+		using _ut = std::underlying_type_t<ENUM>; \
+		return static_cast<ENUM>(static_cast<_ut>(lhs) OP static_cast<_ut>(rhs)); \
+	} \
+	constexpr \
+	ENUM& \
+	operator MTK_CONCAT(OP, =) (ENUM& lhs, ENUM rhs) noexcept \
+	{ \
+		lhs = (lhs OP rhs); \
+		return lhs; \
+	}
+
+#define MTK_DEFINE_FLAG_OPERATORS(ENUM) \
+	static_assert(std::is_enum_v<ENUM>, "ENUM must be an enum class type"); \
+	static_assert(!std::is_convertible_v<ENUM, std::underlying_type_t<ENUM>>, "ENUM must be an enum class type"); \
+	MTK_IMPL_FLAG_OPERATOR(ENUM, |) \
+	MTK_IMPL_FLAG_OPERATOR(ENUM, &) \
+	MTK_IMPL_FLAG_OPERATOR(ENUM, ^) \
+	constexpr ENUM operator~(ENUM rhs) noexcept \
+	{ \
+		using _ut = std::underlying_type_t<ENUM>; \
+		return static_cast<ENUM>(~static_cast<_ut>(rhs)); \
+	}
+
+} // namespace mtk
+
+#endif
