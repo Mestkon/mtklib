6532c66c2b9dc635ca81b292475afa41ea43464d
Mestkon <havhoi92@gmail.com>
2021-05-30 01:26:05 +0200

    Add mtk.hpp

---
+++ include/mtk.hpp
@@ -0,0 +1,14 @@
+#ifndef MTK_HPP
+#define MTK_HPP
+
+//! @file Includes the entire library.
+
+#include <mtk/core.hpp>
+#include <mtk/linalg.hpp>
+#include <mtk/ranges.hpp>
+
+//! All non-macro symbols are defined inside this namespace.
+namespace mtk {
+} // namespace mtk
+
+#endif
