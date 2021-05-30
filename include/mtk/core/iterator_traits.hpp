849ffb2ae671a63f776d2f1c4e1118b93a2ac630
Mestkon <havhoi92@gmail.com>
2021-05-31 01:53:56 +0200

    reformat internal names

--- include/mtk/core/iterator_traits.hpp
+++ include/mtk/core/iterator_traits.hpp
@@ -74,10 +74,10 @@ namespace impl_iterator_traits {
 
 template<class T
 	,class = void>
-struct is_iterator_helper : std::false_type { };
+struct _is_iterator_helper : std::false_type { };
 
 template<class T>
-struct is_iterator_helper<T
+struct _is_iterator_helper<T
 	,_void_t<void
 		,_require<std::is_copy_constructible_v<T>>
 		,_require<std::is_copy_assignable_v<T>>
@@ -96,30 +96,30 @@ struct is_iterator_helper<T
 
 template<class T
 	,class = void>
-struct has_indirection : std::false_type { };
+struct _has_indirection : std::false_type { };
 
 template<class T>
-struct has_indirection<T*> : std::true_type { };
+struct _has_indirection<T*> : std::true_type { };
 
 template<class T>
-struct has_indirection<T
+struct _has_indirection<T
 	,_void_t<void
 		,decltype(mtk::_declval<T&>().operator->())
 	>> : std::true_type { };
 
 template<class T
 	,class = void>
-struct is_input_iterator_helper : std::false_type { };
+struct _is_input_iterator_helper : std::false_type { };
 
 template<class T>
-struct is_input_iterator_helper<T
+struct _is_input_iterator_helper<T
 	,_void_t<void
-		,_require<is_iterator_helper<T>::value>
+		,_require<_is_iterator_helper<T>::value>
 		,_require<std::is_base_of_v<std::input_iterator_tag, iter::iterator_category<T>>>
 		,_require<std::is_convertible_v<decltype(mtk::_declval<const T>() == mtk::_declval<const T>()), bool>>
 		,_require<std::is_constructible_v<bool, decltype(mtk::_declval<const T>() != mtk::_declval<const T>())>>
 		,_require<std::is_same_v<decltype(*mtk::_declval<const T>()), iter::reference<T>>>
-		,_require<has_indirection<T>::value>
+		,_require<_has_indirection<T>::value>
 		,_require<std::is_convertible_v<iter::reference<T>, iter::value_type<T>>>
 		,_require<std::is_convertible_v<decltype(*mtk::_declval<T&>()++), iter::value_type<T>>>
 	>> : std::true_type { };
@@ -129,14 +129,14 @@ struct is_input_iterator_helper<T
 template<class T
 	,class OutputT
 	,class = void>
-struct is_output_iterator_helper : std::false_type { };
+struct _is_output_iterator_helper : std::false_type { };
 
 template<class T
 	,class OutputT>
-struct is_output_iterator_helper<T
+struct _is_output_iterator_helper<T
 	,OutputT
 	,_void_t<void
-		,_require<is_iterator_helper<T>::value>
+		,_require<_is_iterator_helper<T>::value>
 		,_require<std::is_class_v<T> || std::is_pointer_v<T>>
 		,decltype(*(mtk::_declval<T&>()) = mtk::_declval<OutputT>())
 		,_require<std::is_convertible_v<decltype(mtk::_declval<T&>()++), const T&>>
@@ -147,12 +147,12 @@ struct is_output_iterator_helper<T
 
 template<class T
 	,class = void>
-struct is_forward_iterator_helper : std::false_type { };
+struct _is_forward_iterator_helper : std::false_type { };
 
 template<class T>
-struct is_forward_iterator_helper<T
+struct _is_forward_iterator_helper<T
 	,_void_t<void
-		,_require<is_input_iterator_helper<T>::value>
+		,_require<_is_input_iterator_helper<T>::value>
 		,_require<std::is_base_of_v<std::forward_iterator_tag, iter::iterator_category<T>>>
 		,_require<std::is_default_constructible_v<T>>
 		,_require<std::is_same_v<iter::reference<T>, iter::value_type<T>&>
@@ -165,12 +165,12 @@ struct is_forward_iterator_helper<T
 
 template<class T
 	,class = void>
-struct is_bidirectional_iterator_helper : std::false_type { };
+struct _is_bidirectional_iterator_helper : std::false_type { };
 
 template<class T>
-struct is_bidirectional_iterator_helper<T
+struct _is_bidirectional_iterator_helper<T
 	,_void_t<void
-		,_require<is_forward_iterator_helper<T>::value>
+		,_require<_is_forward_iterator_helper<T>::value>
 		,_require<std::is_base_of_v<std::bidirectional_iterator_tag, iter::iterator_category<T>>>
 		,_require<std::is_same_v<decltype(--mtk::_declval<T&>()), T&>>
 		,_require<std::is_convertible_v<decltype(mtk::_declval<T&>()--), const T&>>
@@ -181,12 +181,12 @@ struct is_bidirectional_iterator_helper<T
 
 template<class T
 	,class = void>
-struct is_random_access_iterator_helper : std::false_type { };
+struct _is_random_access_iterator_helper : std::false_type { };
 
 template<class T>
-struct is_random_access_iterator_helper<T
+struct _is_random_access_iterator_helper<T
 	,_void_t<void
-		,_require<is_bidirectional_iterator_helper<T>::value>
+		,_require<_is_bidirectional_iterator_helper<T>::value>
 		,_require<std::is_base_of_v<std::random_access_iterator_tag, iter::iterator_category<T>>>
 		,_require<std::is_same_v<decltype(mtk::_declval<T&>() += mtk::_declval<iter::difference_type<T>>()), T&>>
 		,_require<std::is_same_v<decltype(mtk::_declval<const T>() + mtk::_declval<iter::difference_type<T>>()), T>>
@@ -212,7 +212,7 @@ struct is_random_access_iterator_helper<T
 template<class T>
 struct is_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_iterator_helper<T>
+	: impl_iterator_traits::_is_iterator_helper<T>
 #endif
 { };
 
@@ -225,7 +225,7 @@ struct is_iterator
 template<class T>
 struct is_input_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_input_iterator_helper<T>
+	: impl_iterator_traits::_is_input_iterator_helper<T>
 #endif
 { };
 
@@ -239,7 +239,7 @@ template<class T
 	,class OutputT>
 struct is_output_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_output_iterator_helper<T, OutputT>
+	: impl_iterator_traits::_is_output_iterator_helper<T, OutputT>
 #endif
 { };
 
@@ -252,7 +252,7 @@ struct is_output_iterator
 template<class T>
 struct is_forward_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_forward_iterator_helper<T>
+	: impl_iterator_traits::_is_forward_iterator_helper<T>
 #endif
 { };
 
@@ -265,7 +265,7 @@ struct is_forward_iterator
 template<class T>
 struct is_bidirectional_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_bidirectional_iterator_helper<T>
+	: impl_iterator_traits::_is_bidirectional_iterator_helper<T>
 #endif
 { };
 
@@ -278,7 +278,7 @@ struct is_bidirectional_iterator
 template<class T>
 struct is_random_access_iterator
 #ifndef MTK_DOXYGEN
-	: impl_iterator_traits::is_random_access_iterator_helper<T>
+	: impl_iterator_traits::_is_random_access_iterator_helper<T>
 #endif
 { };
 
