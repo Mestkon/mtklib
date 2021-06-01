b9c6eaa9ed2b4298febbf009add8a8bd7f64f68a
Mestkon <havhoi92@gmail.com>
2021-06-02 00:57:52 +0200

    update doc

--- include/mtk/core/zstring_view.hpp
+++ include/mtk/core/zstring_view.hpp
@@ -1,6 +1,9 @@
 #ifndef MTK_CORE_ZSTRING_VIEW_HPP
 #define MTK_CORE_ZSTRING_VIEW_HPP
 
+//! @file
+//! Contains mtk::zstring_view
+
 #include <mtk/core/assert.hpp>
 #include <mtk/core/types.hpp>
 #include <mtk/core/impl/declval.hpp>
@@ -13,6 +16,9 @@
 
 namespace mtk {
 
+//! @addtogroup core
+//! @{
+
 #ifdef MTK_DEBUG
 constexpr
 size_t
@@ -24,27 +30,50 @@ _constexpr_strlen(const char* s) noexcept
 }
 #endif
 
+//! @brief null-terminated version std::string_view
+//!
+//! @code
+//! #include <mtk/core/zstring_view.hpp>
+//! @endcode
+//!
+//! zstring_view is guaranteed to be either empty, or
+//! null-terminated. If data() is not nullptr, then
+//! data()[size()] is guaranteed to be 0.
 class zstring_view
 {
 public:
+	//! Typedef.
 	using value_type = char;
+	//! Typedef.
 	using pointer = char*;
+	//! Typedef.
 	using const_pointer = const char*;
+	//! Typedef.
 	using reference = char&;
+	//! Typedef.
 	using const_reference = const char&;
+	//! Typedef.
 	using iterator = const_pointer;
+	//! Typedef.
 	using const_iterator = const_pointer;
+	//! Typedef.
 	using size_type = size_t;
+	//! Typedef.
 	using difference_type = ptrdiff_t;
 
+	//! Constructs an empy zstring_view.
 	constexpr
 	zstring_view() noexcept :
 		m_ptr(nullptr),
 		m_size(0)
 	{ }
 
+	//! @brief Constructs a zstring_view from ptr with size count.
+	//!
+	//! @pre ptr != nullptr.
+	//! @pre ptr[count] == 0.
 	constexpr
-	zstring_view(const char* ptr, size_type count) :
+	zstring_view(const_pointer ptr, size_type count) noexcept :
 		m_ptr(ptr),
 		m_size(count)
 	{
@@ -52,7 +81,18 @@ public:
 		MTK_ASSERT(count == _constexpr_strlen(ptr));
 	}
 
-	zstring_view(const char* ptr) :
+	//! @brief Constructs a zstring_view from ptr.
+	//!
+	//! @pre ptr != nullptr.
+	//! @pre ptr must be null-terminated.
+#ifndef MTK_DOXYGEN
+	template<class T
+		,_require<std::is_pointer_v<T>> = 0
+		,_require<std::is_same_v<std::remove_cv_t<std::remove_pointer_t<T>>, char>> = 0>
+	zstring_view(T ptr) :
+#else
+	zstring_view(const_pointer ptr) :
+#endif
 		m_ptr(ptr),
 		m_size(0)
 	{
@@ -60,8 +100,21 @@ public:
 		m_size = std::strlen(ptr);
 	}
 
-	template<class StdString
+	//! @brief Constructs a zstring_view from str.
+	//!
+	//! @pre str[N - 1] == 0.
+	template<size_t N>
+	constexpr
+	zstring_view(const value_type (&str)[N]) noexcept :
+		m_ptr(str),
+		m_size(N - 1)
+	{
+		MTK_ASSERT(str[N - 1] == '\0');
+	}
+
+	//! Constructs a zstring_view from a std::string.
 #ifndef MTK_DOXYGEN
+	template<class StdString
 		//check for random std::string specific interface
 		,_void_t<typename StdString::traits_type>* = nullptr
 		,_void_t<typename StdString::allocator_type>* = nullptr
@@ -73,22 +126,27 @@ public:
 		,_void_t<decltype(StdString::npos)>* = nullptr
 		,_void_t<decltype(mtk::_declval<StdString&>() += mtk::_declval<StdString&>())>* = nullptr
 		,_void_t<decltype(mtk::_declval<StdString&>() += "")>* = nullptr
-#endif
 	>
-	zstring_view(const StdString& s) :
+	zstring_view(const StdString& s) noexcept :
+#else
+	zstring_view(const std::string& s) noexcept :
+#endif
 		m_ptr(s.c_str()),
 		m_size(s.size())
 	{ }
 
-	template<class StdStringView
+	//! Implicit conversion to std::string_view.
 #ifndef MTK_DOXYGEN
+	template<class StdStringView
 		,_void_t<decltype(mtk::_declval<StdStringView&>().remove_prefix(size_t()))>* = nullptr
 		,_void_t<decltype(mtk::_declval<StdStringView&>().remove_suffix(size_t()))>* = nullptr
 		,_void_t<decltype(mtk::_declval<StdStringView&>().substr(size_t()))>* = nullptr
 		,_void_t<decltype(mtk::_declval<StdStringView&>().find_first_not_of(mtk::_declval<StdStringView&>()))>* = nullptr
-#endif
 	>
 	operator StdStringView() const noexcept
+#else
+	operator std::string_view() const noexcept
+#endif
 	{
 		if (!this->empty())
 			return StdStringView(this->data(), this->size() + 1);
@@ -97,6 +155,8 @@ public:
 	}
 
 
+
+	//! Returns iterator to beginning.
 	constexpr
 	const_iterator
 	begin() const noexcept
@@ -104,6 +164,7 @@ public:
 		return this->data();
 	}
 
+	//! Returns iterator to beginning.
 	constexpr
 	const_iterator
 	cbegin() const noexcept
@@ -111,6 +172,7 @@ public:
 		return this->begin();
 	}
 
+	//! Returns iterator to end.
 	constexpr
 	const_iterator
 	end() const noexcept
@@ -118,6 +180,7 @@ public:
 		return this->data() + this->size();
 	}
 
+	//! Returns iterator to end.
 	constexpr
 	const_iterator
 	cend() const noexcept
@@ -125,6 +188,9 @@ public:
 		return this->end();
 	}
 
+	//! @brief Returns *(begin() + pos).
+	//!
+	//! @pre pos < size().
 	constexpr
 	const_reference
 	operator[](size_type pos) const
@@ -133,6 +199,9 @@ public:
 		return *(this->begin() + pos);
 	}
 
+	//! @brief Returns the first character.
+	//!
+	//! @pre !empty().
 	constexpr
 	const_reference
 	front() const
@@ -141,6 +210,9 @@ public:
 		return *this->begin();
 	}
 
+	//! @brief Returns the last non-null character.
+	//!
+	//! @pre !empty().
 	constexpr
 	const_reference
 	back() const
@@ -149,6 +221,7 @@ public:
 		return *(this->end() - 1);
 	}
 
+	//! Returns a pointer to the char array.
 	constexpr
 	const_pointer
 	data() const noexcept
@@ -156,6 +229,9 @@ public:
 		return m_ptr;
 	}
 
+	//! @brief Returns the number of non-null characters in the array.
+	//!
+	//! If size() > 0 then data()[size()] is guaranteed to be 0.
 	constexpr
 	size_type
 	size() const noexcept
@@ -163,6 +239,9 @@ public:
 		return m_size;
 	}
 
+	//! @brief Returns the number of non-null characters in the array.
+	//!
+	//! If length() > 0 then data()[length()] is guaranteed to be 0.
 	constexpr
 	size_type
 	length() const noexcept
@@ -170,6 +249,7 @@ public:
 		return m_size;
 	}
 
+	//! Returns the theoretical maximum size of the stored string.
 	constexpr
 	size_type
 	max_size() const noexcept
@@ -177,6 +257,7 @@ public:
 		return static_cast<size_type>(-1) - 1;
 	}
 
+	//! Returns size() == 0.
 	[[nodiscard]]
 	constexpr
 	bool
@@ -185,6 +266,7 @@ public:
 		return (this->size() == 0);
 	}
 
+	//! Swaps the content of the zstring_views.
 	constexpr
 	void
 	swap(zstring_view& other) noexcept
@@ -198,9 +280,19 @@ private:
 	size_type m_size;
 };
 
+//! @}
+
+
+
+//! @brief Retruns true if the contained c-strings are equivalent else false.
+//!
+//! @relates zstring_view
 bool
 operator==(zstring_view lhs, zstring_view rhs) noexcept;
 
+//! @brief Retruns false if the contained c-strings are equivalent else true.
+//!
+//! @relates zstring_view
 inline
 bool
 operator!=(zstring_view lhs, zstring_view rhs) noexcept
@@ -208,9 +300,17 @@ operator!=(zstring_view lhs, zstring_view rhs) noexcept
 	return !(lhs == rhs);
 }
 
+//! @brief Retruns true if the contained c-string in lhs are
+//! lexicographically less than the one in rhs else false.
+//!
+//! @relates zstring_view
 bool
 operator<(zstring_view lhs, zstring_view rhs) noexcept;
 
+//! @brief Retruns true if the contained c-string in lhs are
+//! lexicographically greater than the one in rhs else false.
+//!
+//! @relates zstring_view
 inline
 bool
 operator>(zstring_view lhs, zstring_view rhs) noexcept
@@ -218,6 +318,10 @@ operator>(zstring_view lhs, zstring_view rhs) noexcept
 	return (rhs < lhs);
 }
 
+//! @brief Retruns true if the contained c-string in lhs are
+//! lexicographically less than or equal to the one in rhs else false.
+//!
+//! @relates zstring_view
 inline
 bool
 operator<=(zstring_view lhs, zstring_view rhs) noexcept
@@ -225,6 +329,10 @@ operator<=(zstring_view lhs, zstring_view rhs) noexcept
 	return !(rhs < lhs);
 }
 
+//! @brief Retruns true if the contained c-string in lhs are
+//! lexicographically greater than or equal to the one in rhs else false.
+//!
+//! @relates zstring_view
 inline
 bool
 operator>=(zstring_view lhs, zstring_view rhs) noexcept
@@ -232,9 +340,15 @@ operator>=(zstring_view lhs, zstring_view rhs) noexcept
 	return !(lhs < rhs);
 }
 
+//! @brief Outputs the string to an std::ostream object.
+//!
+//! @relates zstring_view
 std::ostream&
 operator<<(std::ostream& os, zstring_view zv);
 
+//! @brief Swaps the content of a and b.
+//!
+//! @relates zstring_view
 inline
 void
 swap(zstring_view& a, zstring_view& b) noexcept
