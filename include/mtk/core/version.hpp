36da19aff0603a23a1ad906b82915a824dcf546c
Mestkon <havhoi92@gmail.com>
2021-06-19 04:34:24 +0200

    add doc

--- include/mtk/core/version.hpp
+++ include/mtk/core/version.hpp
@@ -1,10 +1,39 @@
 #ifndef MTK_CORE_VERSION_HPP
 #define MTK_CORE_VERSION_HPP
 
+//! @file
+//! Contains version info.
+
+//! @addtogroup core
+//! @{
+
+//! @brief Major version.
+//!
+//! @code
+//! #include <mtk/core/version.hpp>
+//! @endcode
 #define MTK_MAJOR_VERSION 0
+//! @brief Minor version.
+//!
+//! @code
+//! #include <mtk/core/version.hpp>
+//! @endcode
 #define MTK_MINOR_VERSION 1
 
-//! Combined version. Upper 16 bits is major, lower 16 bits is minor.
-#define MTK_VERSION ((MTK_MAJOR_VERSION << 16) | MTK_MINOR_VERSION)
+//! @brief Combines version to single number.
+//!
+//! @code
+//! #include <mtk/core/version.hpp>
+//! @endcode
+#define MTK_MAKE_VERSION(MAJOR, MINOR) ((MAJOR << 16) | MINOR)
+
+//! @brief Combined version.
+//!
+//! @code
+//! #include <mtk/core/version.hpp>
+//! @endcode
+#define MTK_VERSION (MTK_MAKE_VERSION(MTK_MAJOR_VERSION, MTK_MINOR_VERSION))
+
+//! @}
 
 #endif
